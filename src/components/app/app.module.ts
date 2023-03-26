1206699f7ea7f291a01ebd4d1759760396542104
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-03-26 03:17:04 -0300

    login designed

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoginComponent} from "../login/login.component";
+import { DashboardComponent} from "../dashboard/dashboard.component";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
