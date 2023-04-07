35878ba3ee3826b07f22ecdb65e065d865fc6b97
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-04-06 20:05:18 -0400

    added new components

--- src/components/app.module.ts
+++ src/components/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { LoginComponent} from "../login/login.component";
-import { DashboardComponent} from "../dashboard/dashboard.component";
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginComponent} from "./login/login.component";
+import { DashboardComponent} from "./dashboard/dashboard.component";
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {StoreComponent} from "./store/store.component";
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    StoreComponent,
+    CartComponent
   ],
     imports: [
         BrowserModule,
@@ -23,7 +28,8 @@ import {MatButtonModule} from "@angular/material/button";
         ReactiveFormsModule,
         MatSidenavModule,
         MatIconModule,
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule
     ],
   providers: [],
   bootstrap: [AppComponent]
