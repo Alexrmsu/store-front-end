76e174813e9145bfd539b4304d970dacb437a375
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-04-09 02:28:17 -0400

    minor dashboard adjustments

--- src/components/app/app-routing.module.ts
+++ src/components/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "../dashboard/dashboard.component";
 import {LoginComponent} from "../login/login.component";
+import {StoreComponent} from "../store/store.component";
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'store', component: StoreComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
 
