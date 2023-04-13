b44f5450971e5edba6711d3dc2067c895f103de1
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-04-13 01:12:26 -0400

    management component created

--- src/components/app.module.ts
+++ src/components/app.module.ts
@@ -15,6 +15,7 @@ import { CartComponent } from './cart/cart.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {JWT_OPTIONS, JwtHelperService} from "@auth0/angular-jwt";
 import {TokenInterceptorService} from "../services/token-interceptor.service";
+import { ManagementComponent } from './management/management.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import {TokenInterceptorService} from "../services/token-interceptor.service";
     DashboardComponent,
     LoginComponent,
     StoreComponent,
-    CartComponent
+    CartComponent,
+    ManagementComponent
   ],
     imports: [
         BrowserModule,
