8783dda5f3fdde9ee2d67a5f32954a83f29c0949
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-04-23 02:25:47 -0400

    admin component test and products service

--- src/components/admin/admin.component.ts
+++ src/components/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ProductService} from "../../services/product.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +10,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+
+  productForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private productService: ProductService
+  ) {
+    this.productForm = fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      image: ['', Validators.required],
+    })
+  }
+
+  sendProduct() {
+    console.log(this.productForm.value);
+    this.productService.sendProduct(this.productForm.value).subscribe((res:any) => {
+      if (res.status == 'Product saved') {
+        Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          icon: 'success',
+          title: "Producto creado exitosamente",
+          allowEscapeKey: false,
+          returnFocus: false,
+          timer: 2000,
+          timerProgressBar: true,
+          showConfirmButton: false,
+          didOpen: (toast: HTMLElement) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer);
+            toast.addEventListener('mouseleave', Swal.resumeTimer);
+          }
+        }).fire();
+
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Producto no creado!',
+        })
+      }
+
+    });
+  }
+
+
 
 }
