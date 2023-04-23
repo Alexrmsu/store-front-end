a4c9d1564de830cedb87ec61346b590905e25105
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-04-23 01:47:13 -0400

    routing test

--- src/components/app/app-routing.module.ts
+++ src/components/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "../dashboard/dashboard.component";
 import {LoginComponent} from "../login/login.component";
 import {StoreComponent} from "../store/store.component";
+import {AdminComponent} from "../admin/admin.component";
+import {AuthGuard} from "../../guards/auth.guard";
+import {RoleGuard} from "../../guards/role.guard";
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'store', component: StoreComponent },
+  { path: 'store/:id', component: StoreComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard,RoleGuard], data: {expectedRole: 'admin'}},
   { path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
 
