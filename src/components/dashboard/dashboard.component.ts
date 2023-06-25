d215dee6a9c4af18c6b662a058f64a3fcf74f65c
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-06-24 20:44:37 -0400

    dashboard node api connected via services

--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -1,16 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
   showFiller = false;
+  products: {} = {};
 
   show() {
     this.showFiller = !this.showFiller;
     console.log(this.showFiller)
   }
+
+  constructor(
+    private productService: ProductService,
+  ) {}
+
+  ngOnInit(){
+    this.getProducts()
+  }
+
+
+  getProducts(){
+    this.productService.getProducts().subscribe((res: {}) => {
+      console.log(res);
+      this.products = res;
+    });
+  }
+
+
+
 
 }
